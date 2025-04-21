682ed528fa7296c9db2a22499e04cc4f2aae28c2
rahul-patil-6270 <patilrahul6270@gmail.com>
2025-04-21 20:00:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask,request
+from googlesearch import search 
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(_name_)
+@app.route("/", methods=["POST"])
+def bot():
+    user_msg = request.values.get('Body', '').lower()
+    response = MessagingResponse()
+    q = user_msg + " zpsatara.gov.in"  
+    result = [url for url in search(q, num_results=3)]
+    msg = response.message(f"--- Results for '{user_msg}' ---")
+    for res in result:
+        msg.body(res)  
+    return str(response)
+
+if _name_ == "_main_":
+    app.run()
